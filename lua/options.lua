26f72d000d5aaa5d91a1dceef92eac57ec029df4
krisjanis-a <krishjanis96@gmail.com>
2026-03-02 00:28:36 +0200

    add win32yank clipboard config and update lazy-lock

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,17 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
